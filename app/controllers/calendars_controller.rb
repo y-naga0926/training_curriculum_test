2eb0b872c2942c48a368ccee93331116641fe542
y-naga0926 <75489306+y-naga0926@users.noreply.github.com>
2021-03-24 15:53:34 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,11 +8,12 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
 
-  private
+
 
   def plan_params
     params.require(:calendars).permit(:date, :plan)
